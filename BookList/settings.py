7d7afa3f232a5ffbd7685320857d8c9d4e0e4dcb
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2022-12-30 19:53:44 -0600

    Update settings.py

--- BookList/settings.py
+++ BookList/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ojxl0vxf2u8=2zq&=@7ibytt=pl5e8#b#okas5#&%pn=gf&h+('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
